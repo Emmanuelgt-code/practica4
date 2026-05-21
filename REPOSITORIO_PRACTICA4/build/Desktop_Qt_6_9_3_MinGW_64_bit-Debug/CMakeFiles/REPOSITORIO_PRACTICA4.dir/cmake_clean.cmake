c386567bbedd3b8f909954dbe662c3ea59a258c2
Emmanuelgt-code <emmanuel.guerrat@udea.edu.co>
2026-05-20 20:12:42 -0500

    Primer commit

---
+++ REPOSITORIO_PRACTICA4/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/CMakeFiles/REPOSITORIO_PRACTICA4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/REPOSITORIO_PRACTICA4.dir/main.cpp.obj"
+  "CMakeFiles/REPOSITORIO_PRACTICA4.dir/main.cpp.obj.d"
+  "REPOSITORIO_PRACTICA4.exe"
+  "REPOSITORIO_PRACTICA4.exe.manifest"
+  "REPOSITORIO_PRACTICA4.pdb"
+  "libREPOSITORIO_PRACTICA4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/REPOSITORIO_PRACTICA4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
